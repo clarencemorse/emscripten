26e7be89f0d7781e5bcfe46f42cf4b8595e3899b
Sam Clegg <sbc@chromium.org>
2018-08-23 09:49:10 -0700

    feedback

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -1887,6 +1887,9 @@ void *getBindBuffer() {
   def test_cubegeom_glew(self):
     self.btest('cubegeom_glew.c', reference='cubegeom.png', args=['-O2', '-s', 'LEGACY_GL_EMULATION=1', '-lGL', '-lGLEW', '-lSDL'])
 
+  def test_cubegeom_glew_closure(self):
+    self.btest('cubegeom_glew.c', reference='cubegeom.png', args=['-O2',  '--closure', '1', '-s', 'LEGACY_GL_EMULATION=1', '-lGL', '-lGLEW', '-lSDL'])
+
   @requires_graphics_hardware
   def test_cubegeom_color(self):
     self.btest('cubegeom_color.c', reference='cubegeom_color.png', args=['-s', 'LEGACY_GL_EMULATION=1', '-lGL', '-lSDL'])
